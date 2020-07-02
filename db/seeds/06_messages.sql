e952976b34e92b8caeb5ae3fce83df707aa5b017
eileenlimur <ailin.li@hotmail.com>
2020-07-02 21:37:14 +0000

    made message seeds

---
+++ db/seeds/06_messages.sql
@@ -0,0 +1,5 @@
+INSERT INTO messages (user_id, admin_id, product_id, content, from_user)
+VALUES(1, 1, 2, 'Hey is this still available?', 'true'),
+(1, 1, 2, 'Yes it''s still available!', 'false'),
+(2, 2, 6, 'Hey I''m interested in this artwork', 'true'),
+(2, 2, 6, 'Great! It''s still available. I ship from Toronto. I will send you the order form.', 'false');
